5f2310678f76d7dbfd423f0eb87a2176b32270ca
ellaelisimo <gabriela.barsukova@gmail.com>
2023-03-23 20:41:43 +0200

    events registration

--- back-end/index.js
+++ back-end/index.js
@@ -22,11 +22,33 @@ app.use(registration);
 app.get("/users", async (req, res) => {
   try {
     const con = await mysql.createConnection(MYSQL_CONFIG);
-    const users = `SELECT * FROM users`;
+    const users = `select id, name, surname, email, DATE_FORMAT(date_of_birth, '%Y-%m-%d') as date_of_birth, TIMESTAMPDIFF(YEAR,date_of_birth,CURDATE()) as age from users`;
 
     const result = await con.execute(users);
 
     res.send(result[0]).end();
+  } catch (error) {
+    res.status(500).send(console.error({ error })).end();
+  }
+});
+
+app.get("/events", async (req, res) => {
+  try {
+    const con = await mysql.createConnection(MYSQL_CONFIG);
+
+    const events = (await con.execute("select * from `events`"))[0];
+
+    const eventsWithParticipants = await Promise.all(
+      events.map(async (event) => {
+        const participants = await con.execute(
+          `select users.id, name, surname, email, DATE_FORMAT(date_of_birth, '%Y-%m-%d') as date_of_birth, TIMESTAMPDIFF(YEAR,date_of_birth,CURDATE()) as age, 
+          event_participants.date_of_registration from users inner join event_participants on users.id = event_participants.user_id where event_participants.event_id = ${event.id}`
+        );
+        return { ...event, participants: participants[0] };
+      })
+    );
+
+    res.send(eventsWithParticipants).end();
   } catch (error) {
     res.status(500).send(console.error({ error })).end();
   }
